130531d59cb8f199484f4091b7c709abe743e59c
Daniel Corcoran <danielc.92@outlook.com>
2019-06-14 12:34:44 +1000

    Worked on tables

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
       response => {
         console.log('Intensity');
         console.log(response.data);
-        this.setState({intensity: response.data});
+        this.setState({intensity: response.data.data});
       }
     )
     
@@ -27,7 +27,7 @@ export default class App extends Component {
       response => {
         console.log('Factors');
         console.log(response.data);
-        this.setState({intensity_factors: response.data});
+        this.setState({intensity_factors: response.data.data});
       }
     )
   }
@@ -37,17 +37,77 @@ export default class App extends Component {
     return (
       <section className="section">
         <section className="container">
-        <section className="content">
-          <h2>Carbon Intensity (National, United Kingdom)</h2>
+            <h2>Carbon Intensity (National, United Kingdom)</h2>
+            <hr></hr>
 
-          <h3>Summary</h3>
-          <p>National Grid’s Carbon Intensity API provides an indicative trend of regional carbon intensity of the electricity system in Great Britain (GB) up to 2 days ahead of real-time. It provides programmatic and timely access to both forecast and estimated carbon intensity data. The Carbon Intensity forecast includes CO2 emissions related to electricity generation only. The includes emissions from all large metered power stations, interconnector imports, transmission and distribution losses, and accounts for national electricity demand, embedded wind and solar generation.</p>
+            <h3>Summary</h3>
+            <hr></hr>
+            <p>National Grid’s Carbon Intensity API provides an indicative trend of regional carbon intensity of the electricity system in Great Britain (GB) up to 2 days ahead of real-time. It provides programmatic and timely access to both forecast and estimated carbon intensity data. The Carbon Intensity forecast includes CO2 emissions related to electricity generation only. The includes emissions from all large metered power stations, interconnector imports, transmission and distribution losses, and accounts for national electricity demand, embedded wind and solar generation.</p>
+              
+            <h5>Intensity (/intensity)</h5>
+            <hr></hr>
+            {this.state.intensity.map(item => {return(
+              <React.Fragment>
+              <table>
+              <thead>
+                <tr>
+                  <th>Attribute</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>Date (from)</td>
+                <td>{item['from']}</td>
+              </tr>
+              <tr>
+                <td>Date (to)</td>
+                <td>{item['to']}</td>
+              </tr>
+              {Object.entries(item['intensity']).map(entry => {
+                return (
+                  <React.Fragment>
+                    <tr>
+                      <td>{entry[0]}</td>
+                      <td>{entry[1]}</td>
+                    </tr>
+                  </React.Fragment>
+                  )
+              })}
+              </tbody>
+              </table>
 
-          <h5>Intensity (/intensity)</h5>
+              </React.Fragment>)
+              
+            })
+            }
 
-          <h5>Intensity Factors (/intensity/factors)</h5>
-
-        </section>
+            <h5>Intensity Factors (/intensity/factors)</h5>
+            <hr></hr>
+            <section className="columns is-multiline">
+              {this.state.intensity_factors.map(item => {
+                return <table className="table is-striped">
+                  <thead>
+                    
+                    <tr>
+                      <th>Factor</th>
+                      <th>Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {Object.entries(item).map(entry => { 
+                    return(
+                      <tr>
+                      <td>{entry[0]}</td>
+                      <td>{entry[1]}</td>
+                    </tr>
+                      )
+                  })}
+                  </tbody>
+                </table>
+          
+              })}
+          </section>
         </section>
       </section>
     )
