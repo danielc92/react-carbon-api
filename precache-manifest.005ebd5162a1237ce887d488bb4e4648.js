23664d1eca833da99fc4e53bf7e72eb5a1d7f4c7
Daniel Corcoran <danielc.92@outlook.com>
2019-06-14 21:52:57 +1000

    Updates

---
+++ precache-manifest.005ebd5162a1237ce887d488bb4e4648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96434e41117cc333aa02918d8f6c652",
+    "url": "/react-carbon-api/index.html"
+  },
+  {
+    "revision": "28523b68784ffe66ffde",
+    "url": "/react-carbon-api/static/js/2.cfd35f24.chunk.js"
+  },
+  {
+    "revision": "30020ac765f07e626f06",
+    "url": "/react-carbon-api/static/js/main.5bc91b38.chunk.js"
+  },
+  {
+    "revision": "6bfda22876b37876b9cd",
+    "url": "/react-carbon-api/static/js/runtime~main.2d72c6c3.js"
+  },
+  {
+    "revision": "6231c5fb1903f8c0115d3fe075871ccc",
+    "url": "/react-carbon-api/static/media/undraw_metrics_gtu7.6231c5fb.svg"
+  }
+]);
