2c6c2da7c84a70948983c1d84c236e9701ce78f9
Daniel Corcoran <danielc.92@outlook.com>
2019-06-14 12:00:04 +1000

    Added bulma CSS

--- src/App.js
+++ src/App.js
@@ -35,9 +35,16 @@ export default class App extends Component {
   render() {
 
     return (
-      <div>
-          <h1>Hello World</h1>  
-      </div>
+      <section className="section">
+        <section className="container">
+        <section className="content">
+          <h3>Carbon Intensity UK</h3>
+
+        </section>
+
+        </section>
+
+      </section>
     )
   }
 }
