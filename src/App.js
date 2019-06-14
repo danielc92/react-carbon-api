acbc3d8ea071efe6383f610b8e46d768ec99b581
Daniel Corcoran <danielc.92@outlook.com>
2019-06-14 12:02:44 +1000

    Added summary

--- src/App.js
+++ src/App.js
@@ -38,12 +38,17 @@ export default class App extends Component {
       <section className="section">
         <section className="container">
         <section className="content">
-          <h3>Carbon Intensity UK</h3>
+          <h2>Carbon Intensity (National, United Kingdom)</h2>
+
+          <h3>Summary</h3>
+          <p>National Grid’s Carbon Intensity API provides an indicative trend of regional carbon intensity of the electricity system in Great Britain (GB) up to 2 days ahead of real-time. It provides programmatic and timely access to both forecast and estimated carbon intensity data. The Carbon Intensity forecast includes CO2 emissions related to electricity generation only. The includes emissions from all large metered power stations, interconnector imports, transmission and distribution losses, and accounts for national electricity demand, embedded wind and solar generation.</p>
+
+          <h5>Intensity (/intensity)</h5>
+
+          <h5>Intensity Factors (/intensity/factors)</h5>
 
         </section>
-
         </section>
-
       </section>
     )
   }
