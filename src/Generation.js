b35ff419a2aa7015fb1f49e734d4746bf13eb4c2
Daniel Corcoran <danielc.92@outlook.com>
2019-06-14 14:23:21 +1000

    Added Generation component

---
+++ src/Generation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Generation extends Component {
+    render() {
+        return (
+            <React.Fragment>
+
+                
+            </React.Fragment>
+        )
+    }
+}
