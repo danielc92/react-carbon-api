57b76b307d702a89736845a317057e2d72d30b95
Daniel Corcoran <danielc.92@outlook.com>
2019-06-17 11:31:50 +1000

    Extended summary notes

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -10,7 +10,14 @@ export default class Summary extends Component {
                     <p className="customcode help">Last updated: {this.props.updated}</p>
                     <hr></hr>
                     
-                    <p>This report was built for the purpose of learning how to integrate third party APIs with a React application. The data comes from the <strong>Carbon Intensity API</strong>. The API is ingested using axios library.</p>    
+                    <p>This report was built for the purpose of learning how to integrate third party APIs with a React application. This project takes an open-source approach and costed nothing to create. Key information for this project is listed below:</p>
+                    <ul>
+                        <li>Data from this report is ingested from the Carbon Intensity API, a third party, publicly available API service. The data is live.</li>
+                        <li>Data is processed using Javascripts axios library</li>
+                        <li>React.js was used to build the front-end and handle the API calls.</li>
+                        <li>Chart.js was used to generate the visualizations.</li>
+                        <li>HTML styled with Bulma CSS Framework</li>
+                    </ul>
                 </section>
 
                
