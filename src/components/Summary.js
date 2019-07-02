0e3301d4c0ef584dc230177c4dd9f1a22b3b876f
Daniel Corcoran <danielc.92@outlook.com>
2019-07-02 13:54:47 +1000

    Added button linking to source code

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -7,8 +7,21 @@ export default class Summary extends Component {
                 <section className="container">
                 <section className="content">
                     <h1>Carbon Intensity National Report (United Kingdom)</h1>
-                    <p>By: Daniel Corcoran</p>
-                    <p className="customcode help">Last updated: {this.props.updated}</p>
+                    <div className="columns is-vcentered">
+                        <div className="column">
+                            <p>By: Daniel Corcoran</p>
+                            <p className="customcode help">Last updated: {this.props.updated}</p>
+                        </div>
+                        <div className="column">
+                        <a style={{transition: 'background-color 0.25s ease'}} class="button is-outlined is-primary is-medium" href="https://github.com/danielc92/react-carbon-api.git">
+                        <span class="icon">
+                        <i class="fab fa-github"></i>
+                        </span>
+                        <span>Source Code</span>
+                    </a>
+                        </div>
+                    </div>
+                    
                     <hr></hr>
                     
                     <p>This report was built for the purpose of learning how to integrate third party APIs with a React application. This project takes an open-source approach and costed nothing to create. Key information for this project is listed below:</p>
