28491b0e12a114946b820d3afa7355a2cb7ede14
Daniel Corcoran <danielc.92@outlook.com>
2019-07-02 14:16:30 +1000

    Minor changes to summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -7,31 +7,31 @@ export default class Summary extends Component {
                 <section className="container">
                 <section className="content">
                     <h1>Carbon Intensity National Report (United Kingdom)</h1>
-                    <div className="columns is-vcentered">
-                        <div className="column">
-                            <p>By: Daniel Corcoran</p>
-                            <p className="customcode help">Last updated: {this.props.updated}</p>
-                        </div>
-                        <div className="column">
-                        <a style={{transition: 'background-color 0.25s ease'}} class="button is-outlined is-primary is-medium" href="https://github.com/danielc92/react-carbon-api.git">
-                        <span class="icon">
-                        <i class="fab fa-github"></i>
-                        </span>
-                        <span>Source Code</span>
-                    </a>
-                        </div>
-                    </div>
-                    
+                    <p>By: Daniel Corcoran</p>
+                    <p className="customcode help has-text-primary">Last updated: {this.props.updated}</p>
                     <hr></hr>
+
+                            <p>This report was built for the purpose of learning how to integrate third party APIs with a React application. This project takes an open-source approach and was built at no cost.</p>
+                   
+                            <p>Key information for this project is listed below:</p>
+                            <ul>
+                                <li>Data from this report is ingested from the Carbon Intensity API, a third party, publicly available API service. The data is live.</li>
+                                <li>Data is processed using Javascripts axios library</li>
+                                <li>React.js was used to build the front-end and handle the API calls.</li>
+                                <li>Chart.js was used to generate the visualizations.</li>
+                                <li>HTML styled with Bulma CSS Framework</li>
+                            </ul>
+                    <p>Get the <strong>source code</strong> for this project below.</p>
+                            
+                            <a style={{marginBottom: '1rem', transition: 'background-color 0.25s ease'}} class="button is-outlined is-primary is-medium" href="https://github.com/danielc92/react-carbon-api.git">
+                                <span class="icon">
+                                <i class="fab fa-github"></i>
+                                </span>
+                                
+                                <span>Source Code</span>
+                            </a>
                     
-                    <p>This report was built for the purpose of learning how to integrate third party APIs with a React application. This project takes an open-source approach and costed nothing to create. Key information for this project is listed below:</p>
-                    <ul>
-                        <li>Data from this report is ingested from the Carbon Intensity API, a third party, publicly available API service. The data is live.</li>
-                        <li>Data is processed using Javascripts axios library</li>
-                        <li>React.js was used to build the front-end and handle the API calls.</li>
-                        <li>Chart.js was used to generate the visualizations.</li>
-                        <li>HTML styled with Bulma CSS Framework</li>
-                    </ul>
+                    
                 </section>
                 </section>
             </section>
